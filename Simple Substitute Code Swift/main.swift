3c10e2d6a8910b7cf86de411e18c92602271072f
sergiogoreyko <sergei_goreiko@mail.ru>
2020-06-08 23:03:40 +0300

    Initial Commit

---
+++ Simple Substitute Code Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Simple Substitute Code Swift
+//
+//  Created by Сергей Горейко on 08/06/2020.
+//  Copyright © 2020 Сергей Горейко. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
